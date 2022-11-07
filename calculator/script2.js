7f70182bda01943a67a8d0e021a249a466eb9a7b
Matheus de Oliveira <80687795+omatheuso@users.noreply.github.com>
2022-11-06 22:00:34 -0300

    calculator

---
+++ calculator/script2.js
@@ -0,0 +1,93 @@
+const functionButtons = document.querySelectorAll(['.buttons__clear', '.buttons__backspace', '.buttons__operation'])
+const numericButtons = document.querySelectorAll('.buttons__numeric')
+const display = document.querySelector('.calculator--display')
+
+const math = ['+', '-', '×', '÷']
+const operators = ['C', '←', '=']
+
+let equation = ''
+let currentNumber = '0'
+let equationResult = ''
+
+numericButtons.forEach(
+    el => {
+        el.addEventListener('click', () => {
+            updateCurrentNumber (el.textContent)
+            updateDisplay ()
+        })
+    }
+)
+
+functionButtons.forEach(
+    el => {
+        el.addEventListener('click', () => {
+            switch (el.textContent) {
+                case 'C':
+                    clearAll()
+                    updateDisplay ()
+                    break
+                case '←':
+                    backspace()
+                    updateDisplay ()
+                    break
+                case '=':
+                    solveMath()
+                    updateDisplay ()
+                    break
+                case '+':
+                case '-':
+                case '×':
+                case '÷':
+                    addCurrentNumToEquation(el.textContent)
+                    updateDisplay()
+                    break
+            }
+        })
+    }
+)
+
+function updateDisplay () {
+    display.innerHTML = String(currentNumber).slice(0,10)
+}
+
+function updateCurrentNumber (value) {
+    if (currentNumber === '0') {
+        currentNumber = value
+    }
+    else if (equationResult !== '') {
+        clearAll()
+        currentNumber += value
+    }
+    else {
+        currentNumber += value
+    }
+}
+
+function clearAll () {
+    equation = ''
+    equationResult = ''
+    currentNumber = ''
+}
+
+function backspace () {
+    equation = equation.slice(0,-1)
+    currentNumber = currentNumber.slice(0,-1)
+}
+
+function addCurrentNumToEquation (value) {
+    if (equation.includes('-') || equation.includes('+') || equation.includes('×') || equation.includes('÷')) {
+        solveMath()
+    }
+    else {
+        equation = `${currentNumber}${value}`
+        currentNumber = ''
+    }
+
+}
+
+function solveMath () {
+    equation += currentNumber
+    equationResult = eval(equation.replace('×','*').replace('÷','/'))
+    currentNumber = equationResult
+    equation = ''
+}
---
+++ interactive-rating-component/script.js
@@ -0,0 +1,21 @@
+// const frontPage = document.querySelector(".home--section")
+// const backpage = document.querySelector(".active--section")
+// const meAperta = document.querySelector(".front--btn")
+// const meAperta2 = document.querySelector(".back--btn")
+
+
+// function hideBox () {
+//     frontPage.style.display = "none"
+//     backpage.style.display = "flex"
+// }
+// function hideBox2 () {
+//     frontPage.style.display = "flex"
+//     backpage.style.display = "none"
+// }
+
+// meAperta.addEventListener("click" , function () {
+//     hideBox()
+// })
+// meAperta2.addEventListener("click" , function () {
+//     hideBox2()
+// })
